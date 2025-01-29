1ffc43c3587d20a0ce545b693daff0833f32f764
kousar7 <kousarpashah7@gmail.com>
2025-01-29 09:56:31 +0000

    datatype

--- .devcontainer/01_basic/01_variable.js
+++ .devcontainer/01_basic/01_variable.js
@@ -1,1 +1,19 @@
-const
+const account_id = 2103013
+let accountEmail = "jscodey@gmail.com"
+var accountpassword = "30132102"
+accountcity = "Hyderabad"
+
+let accountstate;
+// account_id =2 // notallowed
+console.log(account_id)
+ accountemail = "xyz@ayz.com"
+accountpassword = "123456"
+accountcity = "Hyderabad city"
+
+ console.table([account_id, accountEmail, accountpassword, accountcity,accountstate])
+
+
+
+
+
+
---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version//
+
+
+//alert(3+3)// we are useing node js, not brower
+console.log(3
+    +3)// code readablity should be high
+    let name = "kouhsar"
+    let age = 25
+    let isLoggedIn = false
+    let state
+    // number = 2to power53
+    // bigint
+    //string => ""
+    //boolen =>  true/false
+    // null =>  standalone value
+    // undfined =>
+    // symbol => unique
+    // object  
+
+    console.log(typeof null); 
+
+
+    
+
