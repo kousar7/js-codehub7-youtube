6614fe4919322538979d4757896123d8e3c55621
kousar7 <kousarpashah7@gmail.com>
2025-01-29 09:57:24 +0000

    datatype

---
+++ .devcontainer/01_basic/03_conversion operation.js
@@ -0,0 +1,26 @@
+let score = 33
+
+"use strictk"; // treat all js code as newer version//
+// alert(3+3) // we are useing nodejs, not browser
+console.log(3
+    +3);  // code readablility should be high separate line
+
+    console.log("kouhsar")
+
+let name =  "kouhsar"
+let age= 25  
+let isloggedin = false
+let state;
+
+// number = 2 to power 53
+//bigint 
+//string =>""
+boolean => true/ false
+//null => standard lones value // object
+//undefined =>//undefined 
+//symbol => unique
+//object 
+console.log(typeof "kouhsar");
+console.log(typeof "age");
+console.log(typeof "undefined");  
+console.log(typeof "null");
